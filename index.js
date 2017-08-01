5a3534638737a685bb4bc5b28f3206acee2d6b91
emile <rudolfsun@gmail.com>
2017-08-01 11:56:00 +0100

    push

---
+++ index.js
@@ -0,0 +1,33 @@
+
+'use strict'
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+//var index = require('./routes/index');
+//var users = require('./routes/users');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set('port', (process.env.PORT || 5000))
+
+//ROUTES
+app.get('/', function(req,res){
+  res.send('Hi I am a chat bot')
+})
+
+//Facebook
+app.get('/webhook/', function (req,res){
+  if (req.query['hub.verify_token'] == ""){
+    res.send(req.query['hub.challenge'])
+  }
+  res.send("Wrong Token")
+})
+
+app.listen(app.get('port'), function(){
+  console.log("running: port")
+})
