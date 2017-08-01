29a2782ef075b696ce11a96a817919cc1fb8a0f0
emile <rudolfsun@gmail.com>
2017-08-01 11:25:21 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+//var index = require('./routes/index');
+//var users = require('./routes/users');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set('port', (process.env.PORT || 5000))
+
+//ROUTES
+app.get('/', function(req,res){
+  res.send('Hi I am a chat bot')
+})
+
+//Facebook
+app.get('/webhook/', function (req,res){
+  if req.query['hub.verify_token'] == ""){
+    res.send(req.query['hub.challenge'])
+  }
+  res.send("Wrong Token")
+})
+
+app.listen(app.get('port'), function(){
+  console.log("running: port")
+})
