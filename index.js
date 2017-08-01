0236cf241b3d15eee80b9ddd7a4352f3b578b9cb
emile <rudolfsun@gmail.com>
2017-08-01 14:07:46 +0100

    enable reply

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ app.get('/', function(req,res){
   res.send('Hi I am a chat bot')
 })
 
+let token = ""
+
 //Facebook
 app.get('/webhook/', function (req,res){
   if (req.query['hub.verify_token'] == "SolutionsStillRock"){
@@ -27,6 +29,40 @@ app.get('/webhook/', function (req,res){
   }
   res.send("Wrong Token")
 })
+
+app.post('/webhook/', function(req, res){
+  let messge_events = req.body.entry[0].message
+    for (let i = 0; i=messge_events.length; i++){
+      let event = messge_events[i]
+      let sender = event.sender.id
+        if (event.message && event.message.text) {
+          let text = event.message.text
+          sendText(sender, "Text echo: " + text.substring(0,100))
+        }
+    }
+    res.sendStatus(200)
+})
+
+
+function sendText(sender, text){
+  let messageData = (text:text)
+    request({
+      url: "https://graph.facebook.com/v2.6/me/messages",
+      qs: {access_token: token},
+      method: "POST",
+      json:{
+        receipt: {id: sender},
+        message: : messageData
+
+      }, function(error, response, body) {
+        if (error) {
+          console.log("sending error")
+        } else if ( response.body.error) {
+            console.log("response body error")
+        }
+      }
+    })
+}
 
 app.listen(app.get('port'), function(){
   console.log("running: port")
