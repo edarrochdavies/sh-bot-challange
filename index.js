c66de209d2655423d367f36f0ff2b6541d045070
emile <rudolfsun@gmail.com>
2017-08-01 14:57:38 +0100

    pushing

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function sendText(sender, text){
       method: "POST",
       json: {
         recipient: {id: sender},
-        message: messageData,
+        message: messageData
       },
        function(error, response, body) {
         if (error) {
